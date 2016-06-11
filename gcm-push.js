ae6abf37756ffa82e60646694b4ec97b1281d564
es <es@evsfy.com>
2016-06-11 19:19:09 +0800

    .

---
+++ gcm-push.js
@@ -0,0 +1,59 @@
+var firebase = require("firebase"),
+    moment = require('moment'),
+    gcm = require('node-gcm');
+
+firebase.initializeApp({
+  databaseURL: "https://chatapp-53089.firebaseio.com/",
+  serviceAccount: "./firebase-cert/ChatApp-cc98693ad5f5.json"
+});
+
+var db = firebase.database();
+var msgRef = db.ref('messages');
+var tokenRef = db.ref('tokens');
+
+
+var tokens = [];
+tokenRef.on("child_added", function(snapshot) {
+        var data = snapshot.val();
+        console.log('added token:', snapshot.key, data);
+        tokens.push(data);
+        console.log(tokens);
+    }
+);
+
+msgRef.orderByChild('time').startAt(
+    starttime()).on("child_added", function(snapshot) {
+        var data = snapshot.val();
+        var title = snapshot.key;
+        console.log('chat message:', data);
+        broadcast(data.message);
+    }
+);
+
+function broadcast(data) {
+    console.log('attempt to send notification:', data);
+    var message = new gcm.Message({
+        priority: "high",
+        notification: {
+            title: "FireChat",
+            body: data.message
+        }
+    });
+    var sender = new gcm.Sender("AIzaSyB0g2jxzqhPTlwZL9AxRvmD0NGbtL-Eg9I");
+    var registrationTokens = {
+        registrationTokens: tokens
+    };
+    sender.send(message, registrationTokens, 10, function (err, response) {
+      if(err) {
+        console.error('push failed:', err);
+      } else {
+        console.log('push success:', response);
+      }
+    });
+}
+
+function starttime () {
+    var start = moment().format('YYYY/MM/DD, hh:mm:ss');
+    console.log('start time:', start);
+    return start;
+}
